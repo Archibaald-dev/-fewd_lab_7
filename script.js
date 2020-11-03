7ef76531aa8cafd55499041783621bd0a56f9dbc
Archibaald-dev <milo.lallement@yahoo.fr>
2020-11-03 17:19:06 +0100

    Code

---
+++ script.js
@@ -0,0 +1,24 @@
+// Q 1
+var firstName = "Milo", favColor = "Blue", favFood = "Salmon", city = "Paris";
+
+//Defines variables, then display them. Here, we are displaying informations one after an other. By replacing the ',' by '+' , it would create a full sentence, with the exact same look, but less space between words. 
+console.log(firstName,",",favColor,",",favFood);
+console.log(firstName+","+favColor+","+favFood);
+
+
+// Q 10
+var phrase =" A rainbow is formed by the sunlight. Shining through rain drops so small. The rain bends the light from the sunshine, and colors the sky for us all.",
+ endingPhrase =" is what we see. Rainbow colours, are made with light bending you see!",
+ one =" Red",
+ two =" orange",
+ three =" yellow",
+ four =" green",
+ five =" blue",
+ six =" indigo",
+ seven =" violet";
+
+console.log(phrase+one+','+two+','+ three+','+ four+','+ five+','+ six+','+ seven+ endingPhrase);
+
+// Q 13
+var myAlphabet = ['A', 'B', 'C', 'D','E','F', 'G'];
+console.log(myAlphabet.length)
